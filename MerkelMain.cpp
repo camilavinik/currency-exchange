1f82b947e66e166d179cb12f07f3de2670d6186b
Camila <contactcamilasanchez@gmail.com>
2023-12-06 19:47:51 -0300

    implement getOrders

--- MerkelMain.cpp
+++ MerkelMain.cpp
@@ -45,6 +45,9 @@ void MerkelMain::printHelp() {
 void MerkelMain::printMarketStats() {
    for (std::string const p : orderBook.getKnownProducts()) {
       std::cout << "Product: " << p << std::endl;
+      std::vector<OrderBookEntry> entries = orderBook.getOrders(OrderBookType::ask, p, "2020/03/17 17:01:24.884492");
+   
+      std::cout << "Asks seen: " << entries.size() << std::endl;
    }
    // std::cout << "OrderBook contains: " << orders.size() << " entries" << std::endl;
    // unsigned int bids = 0;
--- OrderBook.cpp
+++ OrderBook.cpp
@@ -25,5 +25,10 @@ std::vector<std::string> OrderBook::getKnownProducts() {
 
 std::vector<OrderBookEntry> OrderBook::getOrders(OrderBookType type, std::string product, std::string timestamp) {
     std::vector<OrderBookEntry> orders_sub;
+    for (OrderBookEntry& e : orders) {
+        if (e.orderType == type && e.product == product && e.timestamp == timestamp) {
+            orders_sub.push_back(e);
+        }
+    }
     return orders_sub;
 };
