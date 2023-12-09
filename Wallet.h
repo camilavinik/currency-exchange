a208c375900b0402068ec1e0f0de7ed5045f9460
Camila <contactcamilasanchez@gmail.com>
2023-12-09 10:10:44 -0300

    wallet h

---
+++ Wallet.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <map>
+
+class Wallet {
+    public:
+        Wallet();
+        /** insert currency to the wallet */
+        void insertCurrency(std::string type, double amount);
+        /** check if the wallet contains this much currency or more */
+        bool containsCurrency(std::string type, double amount);
+        /** generate string representation */
+        std::string toString();
+
+    private:
+        std::map<std::string, double> currencies;
+}
